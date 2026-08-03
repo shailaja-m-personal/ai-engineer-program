99aa02ebf74b2bb1fa98660d5bf036e5ee1bc98a
jessica-annie-b <jessica.anniebirru@gmail.com>
2026-08-03 17:54:21 -0400

    datatypes examples

---
+++ python-for-ai/datatypes.py
@@ -0,0 +1,14 @@
+# Datatypes
+# 1. Integers
+# 2. float
+# 3. string
+# 4. boolean
+
+a=10
+print(type(a)) #type(input)
+x=10.5
+print(type(x)) #type(input)
+str="Hello"
+print(type(str)) #type(input)
+b=True      
+print(type(b)) #type(input)
